87d520a77314e1cd05d16eb4921a7f2562d4d364
Michael Lindsey <mackeral@gmail.com>
2013-09-11 19:38:56 +0000

    harvester works for all data types

--- harvest.js
+++ harvest.js
@@ -1,9 +1,14 @@
-var allowedFormats = ['oai_dc','simple_dublin_core','qualified_dublin_core','oai_etdms'];
+var allowedFormats = ['oai_dc','simple-dublin-core','qualified-dublin-core','oai_etdms'];
+var baseURL = 'http://scholarship.law.berkeley.edu/do/oai/?verb=ListRecords&';
+var maxRecords =10000;
+var recordType;
+
 if(allowedFormats.indexOf(process.argv[2]) >= 0){
     var http = require('http');
     var parseString = require('xml2js').parseString;
     var records = [];
-    http.get('http://scholarship.law.berkeley.edu/do/oai/?verb=ListRecords&metadataPrefix=' + process.argv[2], function(res){
+    recordType = process.argv[2]
+    http.get(baseURL + 'metadataPrefix=' + recordType, function(res){
         var record;
         res.on('data', function(chunk){ record = record + chunk; });
         res.on('end', function(){
@@ -14,16 +19,44 @@ if(allowedFormats.indexOf(process.argv[2]) >= 0){
                     var recordSet = result['OAI-PMH']['ListRecords'][0]['record'];
                     var resumptionToken = result['OAI-PMH']['ListRecords'][0]['resumptionToken'][0]['_'];
                     recordSet.forEach(function(recordItem){ records.push(JSON.stringify(recordItem)); });
-                    console.log(resumptionToken);
-                    console.log(records.length);
+                    if(resumptionToken){ getMoreRecords(resumptionToken); }
+                    else finish();
                 }
             })
         });
     });
-} else {
-    console.log('usage: node harvest (' + allowedFormats.join('|') + ')')
+} else { console.log('usage: node harvest (' + allowedFormats.join('|') + ')') }
+
+function getMoreRecords(token){
+    console.log(records.length + ' records. getting more with ' + token );
+    http.get(baseURL + 'resumptionToken=' + token, function(res){
+        var record;
+        res.on('data', function(chunk){ record = record + chunk; });
+        res.on('end', function(){
+            var trimmedRecord = record.substring(record.indexOf('<'));
+            parseString(trimmedRecord, { trim: true }, function(err, result){
+                if(err) console.log(err + ":" + result);
+                else {
+                    var recordSet = result['OAI-PMH']['ListRecords'][0]['record'];
+                    var resumptionToken = result['OAI-PMH']['ListRecords'][0]['resumptionToken'][0]['_'];
+                    recordSet.forEach(function(recordItem){ records.push(JSON.stringify(recordItem)); });
+                    if(resumptionToken && (records.length < maxRecords)){ getMoreRecords(resumptionToken); } 
+                    else finish();
+                }
+            })
+        });
+    });
 }
 
-function addRecords(){
-    
+function finish(){ 
+    console.log('done. total of ' + records.length + ' records.');
+    fs = require('fs');
+    today = new Date();
+    var curr_date = ("0" + today.getDate()).slice(-2);
+    var curr_month = ("0" + (today.getMonth() + 1)).slice(-2);
+    var curr_year = today.getFullYear();
+    fs.writeFile('records' + curr_year + curr_month + curr_date + recordType + '.json', JSON.stringify(records), function(err){
+        if(err) console.log('problem writing file:' + err);
+        else console.log('file written');
+    });
 }
